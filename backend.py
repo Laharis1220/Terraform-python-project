25234628584624ecd5c07194e1adec431bea3579
Laharis1220 <81207186+Laharis1220@users.noreply.github.com>
2023-12-01 08:49:13 +0000

    second commit

--- backend.py
+++ backend.py
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "lahari-s3-terraform-2097" 
-#     key            = "lahari/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-lock"
-#   }
-# }
+terraform {
+      backend "s3" {
+      bucket         = "lahari-s3-terraform-2097" 
+      key            = "lahari/terraform.tfstate"
+      region         = "us-east-1"
+      encrypt        = true
+      dynamodb_table = "terraform-lock"
+  }
+ }
