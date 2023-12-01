29a8c81e5fff3d4083164c71cd7e1b939338c547
Laharis1220 <81207186+Laharis1220@users.noreply.github.com>
2023-12-01 10:12:25 +0000

    updated python code

--- main.tf
+++ main.tf
@@ -85,10 +85,13 @@ provisioner "file" {
 provisioner "remote-exec" {
     inline = [
       "echo 'Hello from the remote instance'",
-      "sudo apt update -y",  
-      "sudo apt-get install -y python3-pip", 
-      "sudo pip3 install flask",
       "cd /home/ubuntu",
+      "sudo apt-get update",
+      "sudo apt-get install python3",
+      "pip install virtualenv",
+      "virtualenv python-project",
+      "source python-project/bin/activate",
+      "pip install flask",
       "sudo python3 app.py"
     ]
 }
