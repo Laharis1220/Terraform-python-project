bc043e89d89906a38e4fcc246c7b837a4f6e6703
Laharis1220 <81207186+Laharis1220@users.noreply.github.com>
2023-12-02 19:14:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
-terraform {
+/*terraform {
       backend "s3" {
-      bucket         = "lahari-s3-terraform-20971" 
+      bucket         = "lahari-s3-terraform-1220" 
       key            = "lahari/terraform.tfstate"
       region         = "us-east-1"
       encrypt        = true
       dynamodb_table = "terraform-lock"
    }
   }
+*/
